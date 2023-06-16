aef55a0dcfb0a7fd7fe3634417f9eeb21f274f73
iwan <setyawaniwan853@gmail.com>
2023-06-16 23:03:14 +0700

    progress 1

---
+++ app/src/main/java/com/example/qurbanapp/daftarpenerima/DaftarPenerimaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qurbanapp.daftarpenerima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qurbanapp.R
+
+class DaftarPenerimaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_penerima)
+    }
+}
---
+++ app/src/main/java/com/example/qurbanapp/daftarpenerima/FormTambahPenerimaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qurbanapp.daftarpenerima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qurbanapp.R
+
+class FormTambahPenerimaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_tambah_penerima)
+    }
+}
